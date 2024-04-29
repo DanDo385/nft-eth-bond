880ecd86127b815b14d4a40941e3ba3a70934f7f
Daniel Magro <dando385@yahoo.com>
2024-04-29 15:29:54 -0400

    initial commit

---
+++ compile.py
@@ -0,0 +1,22 @@
+from solcx import compile_standard, install_solc
+
+install_solc('0.8.0')
+
+with open('MyContract.sol', 'r') as file:
+    source_code = file.read()
+
+compiled_sol = compile_standard({
+    "language": "Solidity",
+    "sources": {"MyContract.sol": {"content": source_code}},
+    "settings": {
+        "outputSelection": {
+            "*": {
+                "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
+            }
+        }
+    },
+}, solc_version='0.8.0')
+
+with open("compiled_code.json", "w") as file:
+    import json
+    file.write(json.dumps(compiled_sol))
