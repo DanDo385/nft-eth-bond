dd5996fbee633651a77639815ac7cdcac86dedb5
Daniel Magro <dando385@yahoo.com>
2024-04-29 19:04:20 -0400

    compile.py updated

--- compile.py
+++ compile.py
@@ -2,12 +2,12 @@ from solcx import compile_standard, install_solc
 
 install_solc('0.8.0')
 
-with open('Interaction.sol', 'r') as file:
+with open('BondPriceGenerator.sol', 'r') as file:  # Ensure this file name matches your Solidity file name
     source_code = file.read()
 
 compiled_sol = compile_standard({
     "language": "Solidity",
-    "sources": {"Interaction.sol": {"content": source_code}},
+    "sources": {"BondPriceGenerator.sol": {"content": source_code}},  # Changed from "Interaction.sol"
     "settings": {
         "outputSelection": {
             "*": {
