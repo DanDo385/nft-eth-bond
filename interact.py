23661221938c45fa36662c4575c7ac9eff5dc72d
Daniel Magro <dando385@yahoo.com>
2024-04-29 18:34:27 -0400

    trying to pull in a bond payment schedule

--- interact.py
+++ interact.py
@@ -1,44 +1,19 @@
-from web3 import Web3
-import json
-import sys
+import datetime
 
-# Load the compiled contract ABI
-with open('compiled_code.json', 'r') as file:
-    compiled_sol = json.load(file)
-
-# Load the deployed contract address
-with open('deployed_contract_address.json', 'r') as file:
-    deployed_info = json.load(file)
-contract_address = deployed_info['contract_address']
-
-# ABI from compiled code
-abi = compiled_sol['contracts']['Interaction.sol']['Interaction']['abi']
-
-# Connect to local Ganache
-w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
-
-# Set the default account
-w3.eth.default_account = w3.eth.accounts[0]
-
-# Creating an instance of the deployed contract
-contract = w3.eth.contract(address=contract_address, abi=abi)
-
-# Input from the command line
-if len(sys.argv) != 2:
-    print("Usage: python3 interact_with_contract.py <number>")
-    sys.exit(1)
-
+# Prompt for maturity date of the bond (in years)
 try:
-    new_number = int(sys.argv[1])
+    maturity_years = int(input("Please enter the maturity date of the bond (in years): "))
 except ValueError:
-    print("Error: Please enter a valid integer.")
-    sys.exit(1)
+    print("Error: Please enter a valid integer for maturity years.")
+    exit(1)
 
-# Call the setMyNumber function
-tx_hash = contract.functions.setMyNumber(new_number).transact()
-w3.eth.wait_for_transaction_receipt(tx_hash)
+# Current date
+today = datetime.date.today()
 
-# Call the getMyNumber function
-result = contract.functions.getMyNumber().call()
+# Create payment schedule
+payment_schedule = [today + datetime.timedelta(days=365 * i) for i in range(maturity_years + 1)]
 
-print(f'The number stored in the smart contract is now: {result}')
+# Display the payment schedule
+print("\nBond Payment Schedule:")
+for date in payment_schedule:
+    print(date.strftime("%m/%d/%Y"))
